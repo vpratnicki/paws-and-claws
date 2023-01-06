8a4af8ededf79ccf35dee925b9e74a4bd09a8066
Vanessa Pratnicki <vanessa@pratnicki.net>
2023-01-06 16:18:07 -0500

    Chore: Adjusted body copy.

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -29,7 +29,7 @@ function About() {
             </article>
 
           </div>
-          <p>Tired of struggling to keep your pet's nails trimmed and healthy? Dread the thought of trying to squeeze in a trip to the groomer? Look no further! Our mobile nail trimming service brings the grooming salon to you.</p>
+          <p>Tired of struggling to keep your pet's nails trimmed and healthy? Dread trying to squeeze in a trip to the groomer? Look no further! Our mobile nail trimming service brings the grooming salon to you.</p>
           <p>
           Our professional groomers will come to your home to provide your furry friend with a stress-free nail trimming experience. No need to worry about the hassle of transporting your pet or fitting a grooming appointment into your busy schedule.</p>
           <p>
