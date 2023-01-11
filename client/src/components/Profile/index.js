eeb2263480902218157626f1e95fab5423bc9366
Vanessa Pratnicki <vanessa@pratnicki.net>
2023-01-11 13:10:57 -0500

    Feature: User profile displays on Dashboard.

--- client/src/components/Profile/index.js
+++ client/src/components/Profile/index.js
@@ -1,63 +1,36 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useQuery } from "@apollo/client";
 import { QUERY_ME } from "../../utils/queries";
 
 
 function Profile() {
-  const { data } = useQuery(QUERY_ME);
-  const [users, setUserData] = useState({});
+  const { loading, data } = useQuery(QUERY_ME);
 
-  useEffect(() => {
-    if (data) {
-        console.log(data);
-        setUserData(data.me);
-    }
-}, [data]);
-  
+  if (loading) return <p>Loading...</p>;
+//   console.log(data);
 
   return (
 
-    <h2>Profile</h2>
-    // <div>
-    //     {" "}
-    //   {users.map((val) => {
-    //     return <h1> {val.firstName}</h1>;
-    //   })}
-    // </div>
-
-    // <div className="container services__container-2col">
-
-
-    //     <article className="service">
-    //       <div className="service__head">
-    //         <h3>Profile</h3>
-    //       </div>
-    //       <ul className='service__list'>
-    //         <hr/>
-    //         <li>
-    //           <p>Username</p>
-    //         </li>
-    //         <hr/>
-    //         <li>
-    //           <p>Email</p>
-    //         </li>
-    //         <hr/>
-    //         <li>
-    //           <p>Address</p>
-    //         </li>
-    //         <hr/>
-    //         <li>
-    //           <p>Zip</p>
-    //         </li>
-    //         <hr/>
-    //         <li>
-    //           <p>Phone</p>
-    //         </li>
-    //         <hr/>
-    //       </ul>
-    //     </article>
-    // </div>
-
+      <div className="container services__container-1col">
+        <article className="service">
+        <div className="service__head">
+            <h3>Profile</h3>
+        </div>
+        <ul className='service__list'>
+            <li>Name: {data.me.clientName}</li>
+            <hr/>
+            <li>Username: {data.me.username}</li>
+            <hr/>
+            <li>Email: {data.me.email}</li>
+            <hr/>
+            <li>Phone number: {data.me.phoneNumber}</li>
+            <hr/>
+            <li>Address: {data.me.homeAddress}</li>
+            <hr/>
+            <li>Zip: {data.me.zipcode}</li>
+            </ul>
+        </article>
+    </div>
     );
 }
 
