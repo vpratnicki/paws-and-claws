5aeb023340f0844383f07b3de5abc9430fb8499c
Vanessa Pratnicki <vanessa@pratnicki.net>
2023-01-12 13:51:46 -0500

    Chore: styled add a pet form

--- client/src/components/Dashboard/addpet.js
+++ client/src/components/Dashboard/addpet.js
@@ -1,55 +1,26 @@
 import React, { useState } from "react";
-import Modal from "react-modal";
 import { useMutation } from "@apollo/client";
 import { ADD_PET } from "../../utils/mutations";
 
-// modal styles
-const customStyles = {
-  content: {
-    top: "50%",
-    left: "50%",
-    right: "auto",
-    bottom: "auto",
-    marginRight: "-50%",
-    transform: "translate(-50%, -50%)",
-  },
-};
 
-Modal.setAppElement('#yourAppElement');
+const Addpet = (setIsOpen) => {
 
-const Addpet = () => {
-
-//   let subtitle;
-  const [modalIsOpen, setIsOpen] = React.useState(false);
-
-  function openModal() {
-    setIsOpen(true);
-  }
-
-//   function afterOpenModal() {
-//     subtitle.style.color = '#f00';
-//   }
-
-  function closeModal() {
-    setIsOpen(false);
-  }
-
-  const [formState, setFormState] = useState({
-    petName: "",
-    petType: "",
-  });
-
-  const [addPet, { error }] = useMutation(ADD_PET);
-
-  // update state based on form input changes
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-
-    setFormState({
-      ...formState,
-      [name]: value,
+    const [formState, setFormState] = useState({
+        petName: "",
+        petType: "",
     });
-  };
+
+    const [addPet, { error }] = useMutation(ADD_PET);
+
+    // update state based on form input changes
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+
+        setFormState({
+        ...formState,
+        [name]: value,
+        });
+    };
 
   // add pet submit form
   const handleFormSubmit = async (event) => {
@@ -76,17 +47,9 @@ const Addpet = () => {
   };
 
   return (
-    <div>
-      <h2>Pets</h2>
-        <button onClick={openModal}>Add Pet</button>
-        <Modal
-            isOpen={modalIsOpen}
-            // onAfterOpen={afterOpenModal}
-            onRequestClose={closeModal}
-            style={customStyles}
-            contentLabel="Example Modal"
-        >
-            
+
+
+    <div>            
       <form onSubmit={handleFormSubmit} className="form">
         <div className="form-group">
           <h3>Add a Pet:</h3>
@@ -109,10 +72,9 @@ const Addpet = () => {
             onChange={handleChange}
           />
         </div>
-        <button type="submit">Submit</button>
+        <button className="btn btn-primary" type="submit">Submit</button>
       </form>
       {error && <div>Pet add failed</div>}
-      </Modal>
     </div>
   );
 };
